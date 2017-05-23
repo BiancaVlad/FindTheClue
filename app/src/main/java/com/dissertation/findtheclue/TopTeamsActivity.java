0f960f78ad987f8d44e278298a9023b23d896b09
BiancaVlad <vladbiancalucia@yahoo.com>
2017-05-23 23:05:31 +0300

    Add team and friends to the team view, top teams view

---
+++ app/src/main/java/com/dissertation/findtheclue/TopTeamsActivity.java
@@ -0,0 +1,13 @@
+package com.dissertation.findtheclue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopTeamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_teams);
+    }
+}
